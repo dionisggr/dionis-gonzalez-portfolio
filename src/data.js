5268229b7d40ac6154fd9a67397b532375093b59
Dionis Gonzalez <dionisggr@gmail.com>
2023-12-03 01:39:25 -0700

    adjustments

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const projects = [
   {
-    title: "PromptWiz",
-    description: "A web tool for enhancing ChatGPT and DALL-E prompts, guiding them in providing higher quality and more contextually accurate responses.",
+    title: "SkillClimb",
+    description: "AI-enhanced online course platform",
     technologies: [
       "Vue.js",
       "TailwindCSS",
@@ -18,12 +18,12 @@ const projects = [
       "Vercel",
       "Supabase",
     ],
-    image: "../src/img/promptwiz.png",
-    url: "https://promptwiz.vercel.app/",
+    image: "../src/img/skillclimb.png",
+    url: "https://skillclimb.vercel.app/",
   },
   {
-    title: "ChatterAI",
-    description: "A group chat application that leverages ChatGPT and DALLE models in conversations, as another participant, for helpful discussions and tasks.",
+    title: "PixelHaus",
+    description: "Online renting platform for AI-generated wall art",
     technologies: [
       "React.js",
       "TailwindCSS",
@@ -40,12 +40,12 @@ const projects = [
       "Vercel",
       "Supabase",
     ],
-    image: "../src/img/chatterai.png",
-    url: "https://chatterai-phi.vercel.app",
+    image: "../src/img/pixelhaus.png",
+    url: "https://pixelhaus.vercel.app",
   },
   {
     title: "MultiServe",
-    description: "My personal REST API server for Node.js projects cloned from my PostSign boilerplate.",
+    description: "REST API used for all personal projects",
     technologies: [
       "JavaScript",
       "Nodejs",
@@ -61,26 +61,8 @@ const projects = [
     url: "https://multiserve.up.railway.app/docs",
   },
   {
-    title: "PostSign",
-    description: "A REST API boilerplate for Node.js projects with the most common features already implemented.",
-    technologies: [
-      "JavaScript",
-      "Nodejs",
-      "Express",
-      "Knex",
-      "PostgreSQL",
-      "JWT",
-      "Supabase",
-      "Mailgun",
-      "SwaggerUI",
-      "GPT",
-    ],
-    image: "../src/img/postsign.png",
-    url: "https://github.com/dionisggr/postsign",
-  },
-  {
     title: "FlickShare",
-    description: "Personalized movie suggestions based from full custom lists, and not just one movie!",
+    description: "Personalized movie recommendations from custom lists, not just single films",
     technologies: [
       "React.js",
       "TailwindCSS",
@@ -96,14 +78,14 @@ const projects = [
       "Supabase",
     ],
     image: "../src/img/flickshare.png",
-    url: "https://dionis-gonzalez-portfolio.netlify.app/pong.html",
+    url: "https://flickshare-client.vercel.app/",
     comingSoon: true,
   },
 ];
 const articles = [
   {
-    title: "Starting a Blog in the Era of AI",
-    subtitle: "AN ODYSSEY INTO THE MATRIX",
+    title: "Building Apps with ChatGPT: A Practical Approach",
+    subtitle: "OVERCOMING COMMON CHALLENGES",
     description: "As the world has shifted into hyperdrive with AI, large language models, and the emergence of what I like to call the ‘GPT phenomenon’, it’s become almost a Herculean task to keep up with the sheer pace of technological advancements. It feels a bit like trying to catch a runaway train — but this train’s on an infinite loop, and we’re chasing it on foot. Well, let’s change that narrative.",
     tags: [
       {
@@ -116,62 +98,7 @@ const articles = [
       },
     ],
     image: "../src/img/starting-a-blog.jpeg",
-    url: "https://medium.com/@dionisggr/starting-a-blog-in-the-era-of-ai-bdf17c0160e8?source=your_stories_page-------------------------------------",
-  },
-  {
-    title: "Embracing the Future",
-    subtitle: "NAVIGATING AN ERA OF AI LANGUAGE MODELS",
-    description: "In this constantly evolving panorama of our world, technology has been a pivotal force propelling us into the future. We are living in an era characterized by unprecedented technological advancement and innovation, a time where new technologies are not only emerging but transforming the fabric of our lives, and in a very short amount of time.",
-    tags: [
-      {
-        src: "https://img.shields.io/badge/Technology%20Trends-8E44AD.svg?style=flat",
-        alt: "Technology Trends",
-      },
-      {
-        src: "https://img.shields.io/badge/Artificial%20Intelligence-FF6F00.svg?style=flat",
-        alt: "Artificial Intelligence",
-      },
-    ],
-    image: "../src/img/embracing-the-future.jpeg",
-    url: "https://medium.com/@dionisggr/embracing-the-future-724916203001?source=your_stories_page-------------------------------------",
-  },
-  {
-    title: "Beyond the Interface",
-    subtitle: "UNDERSTANDING MEANINGFUL USER EXPERIENCES",
-    description: "One of the biggest mistakes designers make when designing interfaces is that they often rush straight to the prototype (or drawing board), sketching out how the buttons and screens will look, without first understanding the task and, more importantly, their users better. Think about it, it’s like trying to solve a jigsaw puzzle without having seen the picture on the box, or knowing it’s context.",
-    tags: [
-      {
-        src: 'https://img.shields.io/badge/HCI-663399.svg?style=flat',
-        alt: 'HCI',
-      },
-      {
-        src: 'https://img.shields.io/badge/UX-4D4D4D.svg?style=flat',
-        alt: 'UX',
-      },
-      {
-        src: 'https://img.shields.io/badge/User--Centered%20Design-FFA500.svg?style=flat',
-        alt: 'User-Centered Design',
-      },
-    ],
-    image: "../src/img/beyond-the-interface.jpeg",
-    url: "https://medium.com/@dionisggr/beyond-the-interface-8ee7e68f9433?source=your_stories_page-------------------------------------",
-  },
-  {
-    title: "A Hitchhiker’s Guide to ChatGPT",
-    subtitle: "UNDERSTANDING AI'S TALKATIVE COUSIN",
-    description: "Have you ever wished you had a buddy who could straight up answer your deepest, most random questions at the right time, or make up a captivating story on the spot, maybe even simply keep you company during an insomnia-ridden night? Well, in comes ChatGPT, the charismatic AI that’s been taking the digital world by storm. It’s like the Siri of texting, but with the verve of Shakespeare and the knowledge of a trivia champion.",
-    tags: [
-      {
-        src: 'https://img.shields.io/badge/Artificial_Intelligence-FF6F00.svg?style=flat&logoColor=white',
-        alt: 'Artificial Intelligence',
-      },
-      {
-        src: 'https://img.shields.io/badge/GPT-1F1F1F.svg?style=flat',
-        alt: 'GPT',
-      },
-    ],
-    image: "../src/img/robot.webp",
-    url: "https://medium.com/@dionisggr/a-hitchhikers-guide-to-chatgpt-19e5f4c0be6a?source=your_stories_page-------------------------------------",
+    url: "https://medium.com/design-bootcamp/building-apps-with-chatgpt-a-practical-approach-d8d76c8de239",
   },
   {
     title: "The Art of Prompt Engineering",
@@ -189,6 +116,44 @@ const articles = [
     ],
     image: "../src/img/art-of-prompt-engineering.jpeg",
     url: "https://medium.com/@dionisggr/the-art-of-prompt-engineering-aa07d8c787ca?source=your_stories_page-------------------------------------",
+  },
+  {
+    title: "Navigating the AI Startup Hype",
+    subtitle: "LESSONS FROM THE PAST AND A LOOK INTO THE FUTURE",
+    description: "One of the biggest mistakes designers make when designing interfaces is that they often rush straight to the prototype (or drawing board), sketching out how the buttons and screens will look, without first understanding the task and, more importantly, their users better. Think about it, it’s like trying to solve a jigsaw puzzle without having seen the picture on the box, or knowing it’s context.",
+    tags: [
+      {
+        src: 'https://img.shields.io/badge/HCI-663399.svg?style=flat',
+        alt: 'HCI',
+      },
+      {
+        src: 'https://img.shields.io/badge/UX-4D4D4D.svg?style=flat',
+        alt: 'UX',
+      },
+      {
+        src: 'https://img.shields.io/badge/User--Centered%20Design-FFA500.svg?style=flat',
+        alt: 'User-Centered Design',
+      },
+    ],
+    image: "../src/img/beyond-the-interface.jpeg",
+    url: "https://medium.com/latinxinai/navigating-the-ai-startup-hype-af3b3967a2c",
+  },
+  {
+    title: "A Hitchhiker’s Guide to ChatGPT",
+    subtitle: "UNDERSTANDING AI'S TALKATIVE COUSIN",
+    description: "Have you ever wished you had a buddy who could straight up answer your deepest, most random questions at the right time, or make up a captivating story on the spot, maybe even simply keep you company during an insomnia-ridden night? Well, in comes ChatGPT, the charismatic AI that’s been taking the digital world by storm. It’s like the Siri of texting, but with the verve of Shakespeare and the knowledge of a trivia champion.",
+    tags: [
+      {
+        src: 'https://img.shields.io/badge/Artificial_Intelligence-FF6F00.svg?style=flat&logoColor=white',
+        alt: 'Artificial Intelligence',
+      },
+      {
+        src: 'https://img.shields.io/badge/GPT-1F1F1F.svg?style=flat',
+        alt: 'GPT',
+      },
+    ],
+    image: "../src/img/robot.webp",
+    url: "https://medium.com/@dionisggr/a-hitchhikers-guide-to-chatgpt-19e5f4c0be6a?source=your_stories_page-------------------------------------",
   },
 ];
 
